6de8a91a2635147d7fcea7b48f2b44c8afd34225
Jamie <jamienolan93@hotmail.co.uk>
2021-11-16 21:50:41 +0000

    first commit

---
+++ my_functions.py
@@ -0,0 +1,42 @@
+#function
+
+# def greet():
+#     print("hey!")
+
+# greet()
+
+# def greet(name):
+#     return "hey" + name
+
+# greeting = greet(" Jamie")
+
+# print(greeting)
+
+def greet(name, time_of_day):
+    return "Good " + time_of_day + ", " + name
+
+# greeting = greet("Jamie", "night")
+# print(greeting)
+
+name_1 = "Andrew"
+time_of_day_1 = "morning"
+greeting = greet(name_1, time_of_day_1)
+print(greeting)
+
+name_2 = "Marc"
+time_of_day_2 = "afternoon"
+greeting = greet(name_2, time_of_day_2)
+print(greeting)
+
+# def greet():
+#   words = "Hey"
+#   return words
+
+# def greet_two():
+#   return words
+
+# print(greet_two())
+
+
+
+
